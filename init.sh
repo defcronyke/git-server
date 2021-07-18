00ec73124c0563ca61f1bb324af7c329e79fde0d
Jeremy Carter <jeremy@jeremycarter.ca>
2021-07-18 14:11:12 -0400

    Don't init again if already initialized.

--- init.sh
+++ init.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-if [ -d "git-server" ]; then
-	echo
-	echo "This device has already been initialized. To reinstall, enter the following command instead:"
-	echo
-	echo "  cd git-server; ./install.sh; cd .."
-	echo
-fi
+git_server_init_routine() {
+	if [ -d "git-server" ]; then
+		echo
+		echo "This device has already been initialized. To reinstall, enter the following command instead:"
+		echo
+		echo "  cd git-server; ./install.sh; cd .."
+		echo
+		
+		return 1
+	fi
 
-curl -sL https://gitlab.com/defcronyke/git-server/-/archive/master/git-server-master.tar.gz | tar zxvf - && mv git-server-master git-server && cd git-server && ./install.sh
+	curl -sL https://gitlab.com/defcronyke/git-server/-/archive/master/git-server-master.tar.gz | tar zxvf - && mv git-server-master git-server && cd git-server && ./install.sh
+}
+
+git_server_init_routine
 
