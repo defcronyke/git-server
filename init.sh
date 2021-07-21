5c8337882bccec95d0d0f9b1c306839ca81445f3
Jeremy Carter <jeremy@jeremycarter.ca>
2021-07-21 19:11:48 -0400

    init.sh runs install.sh if device is already initialized.

--- init.sh
+++ init.sh
@@ -3,16 +3,24 @@
 git_server_init_routine() {
 	if [ -d "git-server" ]; then
 		echo
-		echo "This device has already been initialized. To reinstall, enter the following command instead:"
+		echo "This device has already been initialized. (Re-)Installing..."
 		echo
 		echo "  cd git-server; ./install.sh; cd .."
 		echo
+
+    current_dir="$PWD"
+    cd git-server 2>/dev/null
+
+    ./install.sh
+
+    res=$?
+
+    cd "$current_dir"
 		
-		return 1
+		return $res
 	fi
 
 	curl -sL https://gitlab.com/defcronyke/git-server/-/archive/master/git-server-master.tar.gz | tar zxvf - && mv git-server-master git-server && cd git-server && ./install.sh
 }
 
 git_server_init_routine
-
