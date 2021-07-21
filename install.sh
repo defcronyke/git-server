4b53bd2f84ea8ed0b65d26372133dda65c47db8a
Jeremy Carter <jeremy@jeremycarter.ca>
2021-07-21 19:49:26 -0400

    Fix install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git pull
+git pull 2>/dev/null
 if [ $? -ne 0 ]; then
   echo "" && \
   echo "Fetching git-server git repo..." && \
@@ -8,13 +8,13 @@ if [ $? -ne 0 ]; then
   cd ~
 
   if [ ! -d "git-server" ]; then
-    git clone https://gitlab.com/defcronyke/git-server.git || \
+    git clone https://gitlab.com/defcronyke/git-server.git 2>/dev/null || \
     sudo apt-get update && \
     sudo apt-get install -y git && \
-    git clone https://gitlab.com/defcronyke/git-server.git
+    git clone https://gitlab.com/defcronyke/git-server.git 2>/dev/null
     cd git-server
   else
-    cd git-server
+    cd git-server && \
     git pull
   fi
 fi
